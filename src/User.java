8fbc0ebce6ac48945204d220375742d4bf68616a
TARTAIRO <72879269+TARTAIRO@users.noreply.github.com>
2024-02-06 10:24:11 +0100

    Listes des users

--- src/User.java
+++ src/User.java
@@ -1,15 +1,19 @@
 public class User {
-
-    private int id;
     private String email;
+    private String pseudo;
     private String password;
-    private String role;
 
-    public User(int id, String email, String password, String role) {
-        this.id = id;
+    public User(String email, String pseudo, String password) {
         this.email = email;
+        this.pseudo = pseudo;
         this.password = password;
-        this.role = role;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPseudo() {
+        return pseudo;
+    }
 }
